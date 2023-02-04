e535d1dd4fe10aab5909fdb18b21da513e4aeecf
Kingsley Etukudo <kingsleyj90@gmail.com>
2023-02-04 10:37:37 +0100

    fetching data using json server

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new Website", body: "lorem ipsum....", author: "king", id: 1 },
-    {
-      title: "web development",
-      body: "lorem ipsum....",
-      author: "free",
-      id: 2,
-    },
-    { title: "just learning", body: "lorem ipsum....", author: "bro", id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
+  useEffect(() => {
+    fetch(" http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-      <BlogList
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+      {/* <BlogList
         blogs={blogs.filter((blog) => blog.author === "king")}
         title="Kings Blogs"
-      />
+      /> */}
     </div>
   );
 };

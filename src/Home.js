54f8f248d75c1f607b62f52882d3e3153cab7aa5
Kingsley Etukudo <kingsleyj90@gmail.com>
2023-02-04 10:07:32 +0100

    props with function

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlog] = useState([
+  const [blogs, setBlogs] = useState([
     { title: "My new Website", body: "lorem ipsum....", author: "king", id: 1 },
     {
       title: "web development",
@@ -12,14 +13,18 @@ const Home = () => {
     { title: "just learning", body: "lorem ipsum....", author: "bro", id: 3 },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "king")}
+        title="Kings Blogs"
+      />
     </div>
   );
 };
